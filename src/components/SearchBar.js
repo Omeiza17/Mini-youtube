232a0f13e0edaacb0e6c83b831a9b3710559879e
Omeiza17 <smichael15@alustudent.com>
2018-11-18 18:22:12 +0400

    Having issues with the video detail component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+
+  state = {term: ''};
+
+  render() {
+    return (
+        <div className="search-bar ui segment">
+          <form action="" className="ui form" onSubmit={this.onFormSubmit}>
+            <div className="field">
+              <label htmlFor="">
+                Video Search
+              </label>
+              <input type="text" value={this.state.term} onChange={this.onInputChange}/>
+            </div>
+          </form>
+        </div>
+    );
+  }
+
+  onInputChange = event => {
+    this.setState({term: event.target.value})
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
+}
+
+export default SearchBar;
